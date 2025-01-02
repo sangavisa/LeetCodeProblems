9de44f4472ad6e87a999f321f52939dac3eb83ff
SANGAVI ARIVALAGAN <133232436+sangavisa@users.noreply.github.com>
2025-01-02 18:00:30 +0530

    [13] [Time Beats: 62.40%] [Memory Beats: 30.47%] - LeetPush

---
+++ 13-Roman-to-Integer.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap <Character,Integer> hm = new HashMap <>();
+        hm. put('I',1);
+        hm. put('V',5);
+        hm. put('X',10);
+        hm. put('L',50);
+        hm. put('C',100);
+        hm. put('D',500);
+        hm. put('M',1000);
+        int result = 0;
+        for (int i =0;i< s.length();i++){
+            int current=hm.get(s.charAt(i));
+            if (i< s.length()-1 && current<hm.get(s.charAt(i+1))){
+                result-=current;
+            }
+            else{
+                result+=current;
+            }
+        }
+        return result;
+    }
+        
+
+
+
+
+    }
